f7eaf430247b51af996847b73c5112f855a6aeb1
alammari malak <alammarimalak17@gmail.com>
2023-07-25 19:24:10 -0700

    comments added, not working

--- path_man2.c
+++ path_man2.c
@@ -1,14 +1,28 @@
 #include "shell.h"
+
+/*
+ * release_path - function
+ * NULL: NULL
+ */
 void  release_path(void)
 {
 	path_manager(CLEAR_PATH, NULL, NULL);
 }
 
+/*
+ * print_path - function
+ * NULL: NULL
+ */
 void  print_path(void)
 {
 	path_manager(PRINT_PATH, NULL, NULL);
 }
 
+/*
+ * resolve_command_path - function
+ * old path: character
+ * res: integer
+ */
 void resolve_command_path(char **old_path, int *res)
 {
 	path_manager(FIND_CMD, old_path, res);
